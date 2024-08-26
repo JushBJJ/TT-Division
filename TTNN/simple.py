1b153f725bc3b045dfc28b59d1fec22678cfcb81
JushBJJ <36951064+JushBJJ@users.noreply.github.com>
2024-08-26 14:11:59 +1000

    Update TTNN version

--- TTNN/simple.py
+++ TTNN/simple.py
@@ -1,13 +1,6 @@
 import ttnn
 import torch
 
-def ttnn_div(x: ttnn.Tensor, y: ttnn.Tensor) -> ttnn.Tensor:
-    # x * (1 / y) = x / y
-    # return ttnn.multiply(x, ttnn.reciprocal(y))
-    z_0 = ttnn.reciprocal(y)
-    z_1 = ttnn.multiply(x, z_0)
-    return z_1
-
 if __name__ == "__main__":
     device = ttnn.open_device(0)
     try:
@@ -19,10 +12,10 @@ if __name__ == "__main__":
         # Convert torch tensors to TTNN tensors
         y_ttnn = ttnn.from_torch(y, layout=ttnn.TILE_LAYOUT, device=device, dtype=ttnn.bfloat16)
         x_ttnn = ttnn.from_torch(x, layout=ttnn.TILE_LAYOUT, device=device, dtype=ttnn.bfloat16)
-        z_tt = ttnn_div(x_ttnn, y_ttnn ) # TTNN division
+        z_ttnn = ttnn.div(x_ttnn, y_ttnn) # TTNN division
 
         print(f"z_cpu: {z_cpu}")
-        print(f"z_ttnn: {z_tt}")
+        print(f"z_ttnn: {z_ttnn}")
     except Exception as e:
         print(f"Error: {e}")
     finally:
